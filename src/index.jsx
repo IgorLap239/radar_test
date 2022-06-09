db617570a03a82fec48fb7bc409726cbc7da8aed
IgorLap239 <ilaptev@ya.ru>
2022-06-09 11:44:37 +0300

    feat: add ie11

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import 'core-js/stable/symbol'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
